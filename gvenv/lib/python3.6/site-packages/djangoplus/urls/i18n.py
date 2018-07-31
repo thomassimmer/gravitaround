c61b5445a5826e8cf34c521ea87767694b19edb5
Thomas Simmer <thomas.simmer@polyconseil.fr>
2018-07-31 12:41:07 +0200

    deleting stuff

--- gvenv/lib/python3.6/site-packages/djangoplus/urls/i18n.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('djangoplus.views.i18n',
-    url(r'^set-field-trans/$', 'set_field_translation', name='i18n_set_field_translation'),
-)
-
--- gvenv/lib/python3.6/site-packages/djangoplus/widgets/__init__.py
+++
@@ -1,4 +0,0 @@
-from jquery_auto_complete import JQueryAutoComplete
-from ajax_m2m_widget import AjaxMultiSelect, auto_complete_view
-from dropdown_multi_widget import DropDownMultiple
-from raw_id_widget import ModelRawIdWidget
